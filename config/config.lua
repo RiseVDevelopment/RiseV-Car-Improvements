ac08acfff04503efbe5180fd5151ff2f2d39edea
Freakz <chaosfurky1337as@gmail.com>
2025-05-18 17:30:53 +0200

    Create config.lua

---
+++ config/config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+-- Keybinds
+Config.BlinkerLeft = 'LEFT'
+Config.BlinkerRight = 'RIGHT'
+Config.HazardLights = 'UP'
+Config.FensterVorneLinksToggle = '1'
+Config.FensterVorneRechtsToggle = '2'
+Config.FensterHintenLinksToggle = '3'
+Config.FensterHintenRechtsToggle = '4'
+Config.LockToggle = 'U'
+
+-- General Settings
+Config.VehicleDetectionDistance = 7.5
+Config.ActionCooldown = 0.5
+Config.EnableBlinker = true
+Config.EnableFensterSteuerung = true
+Config.EnableLockSystem = true
+Config.EnableSyncBlinker = true
+Config.EffectsOnLock = true
