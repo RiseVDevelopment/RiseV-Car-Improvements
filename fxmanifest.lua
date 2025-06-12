a9abc6652e8998cdda3cdb9dc76d4742e49c912c
Freakz <chaosfurky1337as@gmail.com>
2025-06-12 19:11:41 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 lua54 'yes'
 
 author 'RiseV - Trayx & Freakz'
-description 'Car Improvements'
+description 'RiseV Car Improvements'
 version '1.0.0'
 
 shared_scripts {
@@ -20,3 +20,25 @@ server_scripts {
 client_scripts {
   'client/client.lua'
 }
+
+server_export 'ToggleLock'
+server_export 'GetLockState'
+server_export 'ConsumeLockpick'
+server_export 'ForceUnlock'
+server_export 'CleanVehicle'
+server_export 'RepairVehicle'
+server_export 'SyncBlinker'
+server_export 'TurnOffComponents'
+
+client_export 'ToggleLock'
+client_export 'AttemptLockpick'
+client_export 'ToggleBlinker'
+client_export 'ToggleWindow'
+client_export 'ToggleDoor'
+client_export 'ToggleEngine'
+client_export 'ToggleSeatbelt'
+client_export 'ToggleCruise'
+client_export 'ToggleAutoPilot'
+client_export 'CleanVehicle'
+client_export 'UseRepairkit'
+client_export 'TurnOffComponents'
